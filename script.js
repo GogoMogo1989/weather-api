f762eecc62b99ce33eba062eb3f6e19c40db0043
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-03-28 21:03:42 +0200

    20220328

--- script.js
+++ script.js
@@ -2,40 +2,37 @@ const loadEvent = async _ => {
 
     const apiKey="4da00b046a02fb55e5d68ac66acf1b75"
     const cityInput = document.querySelector("#cityinput")
-    const button = document.querySelector("#button")
-    const cityYouPut = document.querySelector("#cityyouput")
     const description = document.querySelector("#description")
     const temp = document.querySelector("#tempature")
     const wind = document.querySelector("#wind")
     let icon = document.querySelector("#img")
-    let feels = document.querySelector("#feelslike")
+    let hum = document.querySelector("#humidity")
 
-    button.addEventListener('click', function(){
+    cityInput.addEventListener('keyup', function(event){
 
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}`)
-            .then(res => res.json())
-            .then(data => {
+        if(event.keyCode === 13){
 
-                console.log(data)
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}`)
+                .then(res => res.json())
+                .then(data => {
 
-                let nameValue = data["name"]
-                let description2 = data["weather"]["0"]["description"]
-                let tempature = data["main"]["temp"]
-                let windSpeed = data["wind"]["speed"]
-                let icon2 = data["weather"]["0"]["icon"]
-                let feels2 = data["main"]["feels_like"]
+                    console.log(data)
 
-                cityYouPut.innerHTML = nameValue
-                description.innerHTML = description2
-                temp.innerHTML = Math.round(tempature - 273).toFixed(2)
-                wind.innerHTML = windSpeed
-                icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon2}@2x.png`)
-                feels.innerHTML = Math.round(feels2 - 273).toFixed(2)
-             
-            })
+                    let description2 = data["weather"]["0"]["description"]
+                    let tempature = data["main"]["temp"]
+                    let windSpeed = data["wind"]["speed"]
+                    let icon2 = data["weather"]["0"]["icon"]
+                    let hum2 = data["main"]["humidity"]
+                    description.innerHTML = description2
+                    temp.innerHTML = Math.round(tempature - 273).toFixed(2).slice(0, 2)+"°C"
+                    wind.innerHTML = `${windSpeed} km/h`
+                    icon.setAttribute('src', `http://openweathermap.org/img/wn/${icon2}@2x.png`)
+                    hum.innerHTML = `${hum2} %`
+                
+                })
 
-            .catch(err => alert("Wrong city name, please choice to other city!"))
-
+                .catch(err => alert("Wrong city name, please choose than other city!"))
+        }
     })
 
 }
